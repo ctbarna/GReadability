0127b9051f43dea83b80fc46478f78386844b79a
Chris Barna <chris@unbrain.net>
2011-02-21 20:41:44 -0800

    Initial import of the GReadability user script. No active code, just headers.

---
+++ GReadability.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name GReadability
+// @namespace http://www.github.com/ctbarna
+// @description Retrieve the full text of a specific Google Reader article.
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @include http://www.google.com/reader/*
+// ==/UserScript==
