806e346988fbe7fa5acc5916a2a3afba5c9dbbec
Chris Barna <chris@unbrain.net>
2011-02-21 21:06:38 -0800

    That hack only works in Firefox. Using http://erikvold.com/blog/index.cfm/2010/6/14/using-jquery-with-a-user-script

--- GReadability.user.js
+++ GReadability.user.js
@@ -2,44 +2,30 @@
 // @name GReadability
 // @namespace http://www.github.com/ctbarna
 // @description Retrieve the full text of a specific Google Reader article.
-// @require http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
 // @include http://www.google.com/reader/*
 // ==/UserScript==
-var $;
-
-// Add jQuery
-(function(){
-  if (typeof unsafeWindow.jQuery == 'undefined') {
-    var GM_Head = document.getElementsByTagName('head')[0] || document.documentElement,
-        GM_JQ = document.createElement('script');
-
-    GM_JQ.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
-    GM_JQ.type = 'text/javascript';
-    GM_JQ.async = true;
-
-    GM_Head.insertBefore(GM_JQ, GM_Head.firstChild);
-  }
-  GM_wait();
-})();
-
-// Check if jQuery's loaded
-function GM_wait() {
-  if (typeof unsafeWindow.jQuery == 'undefined') {
-    window.setTimeout(GM_wait, 100);
-  } else {
-    $ = unsafeWindow.jQuery.noConflict(true);
-    letsJQuery();
-  }
+function addJQuery(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    script.textContent = "(" + callback.toString() + ")();";
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
 }
 
-// All your GM code must be inside this function
-function letsJQuery() {
+// The guts of this userscript
+function main() {
   var API_URL = "http://plateboiler.lazyreadr.com/";
-
-  $(document).keydown(function (event) {
+  alert("Works");
+  $.keydown(function (event) {
     if (event.keyCode == '48') {
       var link = $(".entry-container .entry-title-link").attr("href");
       alert(link);
     }
   });
 }
+
+// load jQuery and execute the main function
+addJQuery(main);
