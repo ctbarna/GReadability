cceadb4a690328dbcbbb3babc4543daa7de1633c
Chris Barna <chris@unbrain.net>
2011-02-21 20:47:59 -0800

    Changed jQuery version to 1.3.2 because all future versions don't work. May change to the hack at http://joanpiedra.com/jquery/greasemonkey/ in the future.

--- GReadability.user.js
+++ GReadability.user.js
@@ -2,6 +2,8 @@
 // @name GReadability
 // @namespace http://www.github.com/ctbarna
 // @description Retrieve the full text of a specific Google Reader article.
-// @require http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
 // @include http://www.google.com/reader/*
 // ==/UserScript==
+
+
