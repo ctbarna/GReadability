aacc30a922ade766a9fb6daa1305538644649422
Chris Barna <chris@unbrain.net>
2011-02-21 20:56:35 -0800

    Changed to the hack, started to add functional code.

--- GReadability.user.js
+++ GReadability.user.js
@@ -5,5 +5,41 @@
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
 // @include http://www.google.com/reader/*
 // ==/UserScript==
+var $;
 
+// Add jQuery
+(function(){
+  if (typeof unsafeWindow.jQuery == 'undefined') {
+    var GM_Head = document.getElementsByTagName('head')[0] || document.documentElement,
+        GM_JQ = document.createElement('script');
 
+    GM_JQ.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
+    GM_JQ.type = 'text/javascript';
+    GM_JQ.async = true;
+
+    GM_Head.insertBefore(GM_JQ, GM_Head.firstChild);
+  }
+  GM_wait();
+})();
+
+// Check if jQuery's loaded
+function GM_wait() {
+  if (typeof unsafeWindow.jQuery == 'undefined') {
+    window.setTimeout(GM_wait, 100);
+  } else {
+    $ = unsafeWindow.jQuery.noConflict(true);
+    letsJQuery();
+  }
+}
+
+// All your GM code must be inside this function
+function letsJQuery() {
+  var API_URL = "http://plateboiler.lazyreadr.com/";
+
+  $(document).keydown(function (event) {
+    if (event.keyCode == '48') {
+      var link = $(".entry-container .entry-title-link").attr("href");
+      alert(link);
+    }
+  });
+}
